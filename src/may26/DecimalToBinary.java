4715220a65f2c53ccaa04fa886d48c854137a35d
ARSH <arshrakholiya3@gmail.com>
2026-05-26 12:19:49 +0530

    hexa to decimal

--- src/may26/DecimalToBinary.java
+++ src/may26/DecimalToBinary.java
@@ -33,4 +33,5 @@ public class DecimalToBinary {
         }
         return total;
     }
+
 }
--- src/may26/HexaToDecimal.java
+++ src/may26/HexaToDecimal.java
@@ -6,17 +6,27 @@ public class HexaToDecimal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        System.out.println(decimalConversionFromHexa("1C3"));
         sc.close();
     }
 
-//    static int decimalConversionFromHexa (String s) {
-//
-//        int i = s.length() - 1, result = 0, power = 1;
-//
-//        while (i >= 0) {
-//            result +=
-//        }
-//
-//    }
+    static int decimalConversionFromHexa (String s) {
+
+        int i = s.length() - 1, result = 0, power = 1, rem = 0;
+
+        while (i >= 0) {
+            char ch = s.charAt(i);
+            if (ch >= '0' && ch <= '9') {
+                rem = ch - '0';
+            }
+            else {
+                rem = (ch - 'A') + 10;
+            }
+            result += (rem * power);
+            power *= 16;
+            i--;
+        }
+
+        return result;
+    }
 }
