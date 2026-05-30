fa78c23239945aa340ba3c19c6689395077abd4a
ARSH <arshrakholiya3@gmail.com>
2026-05-30 11:33:48 +0530

    nothing

---
+++ src/Array/may30/SubtractionOfArrayOfItsDigits.java
@@ -0,0 +1,12 @@
+package Array.may29;
+
+import java.util.*;
+
+public class SubtractionOfArrayOfItsDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        sc.close();
+    }
+}
